098ae418d06c257bf1ecc433e1552d049efabf4d
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-06-19 14:58:02 +0200

    Removed __pacache__ dirs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 from routes.UserRouter import router as userRouter
 
 app = FastAPI()
+
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 origins = ["https://localhost:3000"]
 
@@ -20,8 +23,8 @@ app.include_router(
   prefix="/api"
 )
 
-@app.get("/api") 
-def hello():
-  return { "message": "Hello, world!!" }
+@app.get("/api/items") 
+async def read_items(token: str = Depends(oauth2Scheme)):
+  return { "token": token }
 
 app.mount("/", StaticFiles(directory="client/build", html=True), name="client")
